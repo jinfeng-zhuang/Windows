0ef1dedd54d8abb5e592fe59247830fa6c51965c
Jinfeng Zhuang <Jinfengz@v-silicon.com>
2019-07-03 19:52:51 +0800

    HTML preprocessor add, now it works for many HTML
    Signed-off-by: Jinfeng Zhuang <Jinfengz@v-silicon.com>

--- Source/CustomWindowClasses/HTMLBoard.cpp
+++ Source/CustomWindowClasses/HTMLBoard.cpp
@@ -1,16 +1,30 @@
+//
+// 多行显示一段数据的算法：
+// 输入：一段文字，当前设备环境
+// 输出：INDEX 1 INDEX 2 INDEX 3
+// 1. 统计单词数量
+// 2. 如果加上下一个单词的长度大于一行的总长度，就把它放在下一行
+// 3. 如果一个单词的长度本来就比行要大，不需要连接符，直接把多余的部分放到下一行
+// 
+// 数据流：
+// 1. 获取用户传入数据的大小，申请两块 BUFFER
+// 2. 拷贝用户传入的数据进 BUFFER A，解析第一次，不管属性，得到元素数量，拷贝是为了断开与调用者的联系
+// 3. 第二次解析前，申请正好合适的属性和数据空间，解析后放到 BUFFER ATTR 和 BUFFER DATA 后，释放 BUFFER A
+//
+// 提示：
+// 其实应该有第一次获取元素数量，确定需要申请的内存大小，但是消耗了时间；
+// 第二次要获取更加详细的数据，比如属性值，这就太多了，没法在第一次分配完；
+// 我更倾向于一次拿到一个完整的 BUFFER ， DUMP 数据的时候也很方便，自己的东西自己管。
+// 属性是固定的，总空间也是固定的，单个数据长度是可变的，但是整体是不变的。
+//
+
 #include <Windows.h>
 #include "common.h"
 #include "tinyxml2.h"
 
 // stored as ANSI, but restored with Unicode, I don't know why
 // there is a c code to copy data from harddisk to ddr, maybe the c code decode this
-static const wchar_t html[] = L"<html><body><h1>Header1</h1><p>Hello 你好 HTML!</p></body></html>";
-
-static wchar_t unicode_buffer[1024];
-static int unicode_buffer_length = 1024;
-
-static wchar_t utf8_buffer[1024];
-static int utf8_buffer_length = 1024;
+static wchar_t* html;
 
 // css table
 // global: consolas
@@ -23,29 +37,54 @@ static int utf8_buffer_length = 1024;
 struct attr {
     const char name[32];
     int fontsize;
+    unsigned int bgcolor;
+    unsigned int fgcolor;
 };
 
 struct attr attr_map[] = {
-    {"h1", 50},
-    {"h2", 40},
-    {"h3", 30},
-    {"p", 20},
+    {"h1", 50, 0xFFFFFF, 0x000000},
+    {"h2", 40, 0xFFFFFF, 0x000000},
+    {"h3", 30, 0xFFFFFF, 0x000000},
+    {"p", 20, 0xFFFFFF, 0x000000},
+    {"a", 20, 0xFFFFFF, 0xFF0000},
+    {"span", 20, 0xFFFFFF, 0x000000},
 };
 
-int draw_text(HDC hdc, RECT rect, COLORREF bgcolor, COLORREF fgcolor, int fontsize, SIZE *size)
+int get_one_line(HDC hdc, const WCHAR* text, RECT rect)
+{
+    int i;
+    SIZE size;
+    int line_size = rect.right - rect.left;
+
+    for (i = 0; i < lstrlen(text); i++) {
+        GetTextExtentPoint(hdc, text, i, &size);
+        if (size.cx >= line_size) {
+            break;
+        }
+    }
+
+    if (i == lstrlen(text))
+        return 0;
+
+    return i - 1;
+}
+
+int draw_text(HDC hdc, const WCHAR *text, RECT *rect, COLORREF bgcolor, COLORREF fgcolor, int fontsize)
 {
     DRAWTEXTPARAMS drawtextparams;
     LOGFONT logicfont;
     HFONT hfont;
     int ret;
-    int i;
+    SIZE size;
+    int start;
 
     memset(&logicfont, 0, sizeof(logicfont));
     wsprintf(logicfont.lfFaceName, TEXT("Consolas"));
     logicfont.lfHeight = fontsize;
     hfont = CreateFontIndirect(&logicfont);
     SelectObject(hdc, hfont);
-    GetTextExtentPoint(hdc, html, lstrlen(html), size);
+
+    GetTextExtentPoint(hdc, L"X", lstrlen(L"X"), &size);
 
     SetBkColor(hdc, bgcolor);
     SetTextColor(hdc, fgcolor);
@@ -55,9 +94,95 @@ int draw_text(HDC hdc, RECT rect, COLORREF bgcolor, COLORREF fgcolor, int fontsi
     drawtextparams.iLeftMargin = 1;
     drawtextparams.iRightMargin = 1;
 
-    DrawTextEx(hdc, (wchar_t*)html, -1, &rect, DT_WORDBREAK | DT_EXPANDTABS, &drawtextparams);
+    start = 0;
+
+    while (1) {
+        ret = get_one_line(hdc, &text[start], *rect);
+
+        DrawTextEx(hdc, (wchar_t *)&text[start], ret ? ret : -1, rect, DT_EXPANDTABS, &drawtextparams);
+
+        rect->top += size.cy;
+
+        if (0 == ret)
+            break;
+
+        start += ret;
+    }
 
     return 0;
+}
+
+// accept only unicode
+void HTMLShow(HWND hwnd, const WCHAR *text)
+{
+    RECT rect;
+
+    html = (WCHAR *)text;
+
+    GetClientRect(hwnd, &rect);
+    InvalidateRect(hwnd, &rect, true);
+}
+
+void node_scan(HDC hdc, tinyxml2::XMLElement* node, unsigned char *buffer, int buffer_length, RECT *rect)
+{
+    int i;
+
+    if (node) {
+        printf("node %s\n", node->Value());
+        for (i = 0; i < sizeof(attr_map) / sizeof(attr_map[0]); i++) {
+            if (0 == strcmp(node->Value(), attr_map[i].name)) {
+                memset(buffer, 0, buffer_length);
+                if (NULL != node->GetText()) {
+                    MultiByteToWideChar(CP_UTF8, 0, (char*)node->GetText(), strlen(node->GetText()), (wchar_t*)buffer, buffer_length);
+                    draw_text(hdc, (wchar_t*)buffer, rect, attr_map[i].bgcolor, attr_map[i].fgcolor, attr_map[i].fontsize);
+                }
+            }
+        }
+
+        node_scan(hdc, node->FirstChildElement(), buffer, buffer_length, rect);
+        node_scan(hdc, node->NextSiblingElement(), buffer, buffer_length, rect);
+    } 
+}
+
+static void preprocess(char* src, char* dest)
+{
+    int i;
+    int j;
+    int tmp;
+    int slash_flag = false;
+
+    for (i = 0, j = 0; i < strlen(src); i++) {
+
+        if (src[i] == '<') {
+            if ((0 == memcmp(&src[i], "<img", 4)) ||
+                (0 == memcmp(&src[i], "<br", 3)) ||
+                (0 == memcmp(&src[i], "<link", 5)) ||
+                (0 == memcmp(&src[i], "<meta", 5)) ||
+                (0 == memcmp(&src[i], "<input", 6))) {
+                tmp = i;
+                while (src[tmp++] != '>');
+                if (src[tmp - 2] != '/') {
+                    slash_flag = true;
+                }
+            }
+
+            if (0 == memcmp(&src[i], "<script", 7)) {
+                for (tmp = i + 7; ; tmp++) {
+                    if (0 == memcmp(&src[tmp], "</script>", 9)) {
+                        i = tmp + 9;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (src[i] == '>' && slash_flag) {
+            slash_flag = false;
+            dest[j++] = '/';
+        }
+
+        dest[j++] = src[i];
+    }
 }
 
 static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -65,21 +190,14 @@ static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM l
 	HDC hdc;
 	PAINTSTRUCT ps;
 	HINSTANCE hInstance;
-	TEXTMETRICW tm;
-    RECT rect;
     RECT clientrect;
-    DRAWTEXTPARAMS drawtextparams;
-    COLORREF colorref;
-    COLORREF bgcolor;
-    LOGFONT logicfont;
-    HFONT hfont;
+    RECT rect;
     tinyxml2::XMLDocument doc;
-    tinyxml2::XMLElement *root;
-    tinyxml2::XMLElement *body;
-    tinyxml2::XMLElement*node;
-    int ret;
-    int i;
-    SIZE size;
+    tinyxml2::XMLElement *root = NULL;
+    unsigned char* buffer_orig = NULL;
+    unsigned char* buffer = NULL;
+    int buffer_lenth;
+    FILE* fp = NULL;
 
 	//printf("Window Message: 0x%04X\n", uMsg);
 
@@ -88,33 +206,6 @@ static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM l
 	switch (uMsg)
 	{
 	case WM_CREATE:
-
-        // detect the encode format
-
-        // decode the ASCII/ANSI/UTF-8/... to UNICODE
-
-        //ret = MultiByteToWideChar(CP_ACP, 0, (char *)html, lstrlen(html), unicode_buffer, unicode_buffer_length);
-        //ret = WideCharToMultiByte(CP_UTF8, 0, unicode_buffer, ret, (char *)utf8_buffer, utf8_buffer_length, NULL, NULL);
-        ret = WideCharToMultiByte(CP_UTF8, 0, html, lstrlen(html), (char *)utf8_buffer, utf8_buffer_length, NULL, NULL);
-        (void)ret;
-        doc.Parse((const char *)utf8_buffer);
-        root = doc.RootElement();
-        if (root) {
-            body = root->FirstChildElement("body");
-            if (body) {
-                node = body->FirstChildElement();
-                while (node) {
-                    printf("%s: %s\n", node->Value(), node->GetText());
-                    for (i = 0; i < sizeof(attr_map) / sizeof(attr_map[0]); i++) {
-                        if (0 == strcmp(node->Value(), attr_map[i].name)) {
-                            printf("size = %d\n", attr_map[i].fontsize);
-                        }
-                    }
-                    node = node->NextSiblingElement();
-                }
-            }
-        }
-
 		break;
 	case WM_SIZE:
 		break;
@@ -127,12 +218,54 @@ static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM l
 
         GetClientRect(hWnd, &clientrect);
 
-        draw_text(hdc, clientrect, 0xFFFFFF, 0xFF0000, 50, &size);
+        rect.left = (clientrect.right - clientrect.left - 800) / 2;
+        rect.right = rect.left + 800;
+        rect.top = 20;
 
-        rect = clientrect;
-        rect.top = 200;
+        buffer_lenth = lstrlen(html) * 2;
 
-        draw_text(hdc, rect, 0xFFFFFF, 0x0000FF, 20, &size);
+        buffer_orig = (unsigned char*)malloc(buffer_lenth);
+        if (NULL == buffer_orig)
+            goto END_PAINT;
+
+        memset(buffer_orig, 0, buffer_lenth);
+
+        buffer = (unsigned char*)malloc(buffer_lenth);
+        if (NULL == buffer)
+            goto END_PAINT;
+
+        memset(buffer, 0, buffer_lenth);
+
+        WideCharToMultiByte(CP_UTF8, 0, html, lstrlen(html), (char*)buffer_orig, buffer_lenth, NULL, NULL);
+
+        preprocess((char *)buffer_orig, (char*)buffer);
+
+        fp = fopen("debug.html", "w");
+        fwrite(buffer, 1, strlen((char*)buffer), fp);
+        fflush(fp);
+        fclose(fp);
+
+        doc.Parse((const char*)buffer);
+
+        root = doc.RootElement();
+        if (root) {
+            node_scan(hdc, root, buffer, buffer_lenth, &rect);
+        }
+        else {
+            buffer = (unsigned char*)malloc(1024);
+            if (NULL != buffer) {
+                memset(buffer, 0, 1024);
+                MultiByteToWideChar(CP_UTF8, 0, (char*)doc.ErrorStr(), strlen(doc.ErrorStr()), (wchar_t*)buffer, 1024);
+                draw_text(hdc, (wchar_t*)buffer, &rect, 0xFFFFFF, 0x000000, 20);
+            }
+        }
+
+    END_PAINT:
+        if (buffer_orig)
+            free(buffer_orig);
+
+        if (buffer)
+            free(buffer);
 
 		EndPaint(hWnd, &ps);
 		break;
--- Source/WinMain.cpp
+++ Source/WinMain.cpp
@@ -4,19 +4,25 @@ extern void serial_port_list(void);
 extern void clipboard_list(void);
 extern const wchar_t* GetPredefinedClipboardFormatName(UINT format);
 
+extern void HTMLShow(HWND hwnd, const WCHAR* text);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	MSG msg;
 	HWND hwnd;
+    FILE* fp;
+    int filesize;
+    char* content;
+    wchar_t* unicode_content;
 
-    AllocConsole();
-    freopen("CONOUT$", "w", stdout);
+    //AllocConsole();
+    //freopen("CONOUT$", "w", stdout);
     //freopen("log.txt", "w", stdout);
 
 	// Custom Window Class Register
 	CustomWindowClassesInit(hInstance);
 
-	clipboard_list();
+	//clipboard_list();
 
 	// Custom Window Create
 	hwnd = CreateWindowEx(
@@ -24,11 +30,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		TEXT("HTMLBoard"),
 		TEXT("Demo"),
 		WS_OVERLAPPEDWINDOW | WS_VSCROLL| WS_HSCROLL,
-		0, 0, 1000, 800,
+		300, 200, 1000, 400,
 		NULL, // hWndParent
 		NULL, // hMenu
 		hInstance,
 		NULL);
+
+    fp = fopen("demo.html", "r");
+    if (NULL != fp) {
+        fseek(fp, 0, SEEK_END);
+        filesize = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        content = (char*)malloc(filesize + 1);
+        memset(content, 0, filesize + 1);
+
+        fread(content, 1, filesize, fp);
+
+        unicode_content = (wchar_t*)malloc(filesize * 2);
+        memset(unicode_content, 0, filesize * 2);
+
+        MultiByteToWideChar(CP_UTF8, 0, (char *)content, strlen(content), unicode_content, filesize * 2);
+
+        HTMLShow(hwnd, (const wchar_t *)unicode_content);
+    }
+    else {
+        HTMLShow(hwnd, L"<html><body><p>File not found!</p></body></html>");
+    }
 
 	ShowWindow(hwnd, nShowCmd);
 	UpdateWindow(hwnd);
